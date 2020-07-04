1a136c04b9b9b71884bb074552b3add4ff0e2d8f
Abdullah <aalthawr@gmail.com>
2020-07-04 15:47:49 +0800

    Add tabs

--- client/Studio.js
+++ client/Studio.js
@@ -1,8 +1,34 @@
-import React from "react"
+import React, { useState, useContext } from "react"
 import * as ReactDOM from "react-dom"
+import Tabs from "@atlaskit/tabs"
+import VideosList from "./Components/VideosList"
+import UploadVideo from "./Components/UploadVideo"
+
+const TabSelectContext = React.createContext(null)
+export const useTabSelectContext = () => useContext(TabSelectContext)
 
 const Studio = () => {
-  return <div>Hello From Studio</div>
+  const [selectedTab, setSelectedTab] = useState({ tab: 0 })
+
+  const tabs = [
+    {
+      label: "Upload Video",
+      content: <UploadVideo />,
+    },
+    { label: "Videos", content: <VideosList /> },
+  ]
+
+  return (
+    <div>
+      <TabSelectContext.Provider value={{ selectedTab, setSelectedTab }}>
+        <Tabs
+          onSelect={(tab, index) => setSelectedTab({ tab: index })}
+          selected={tabs[selectedTab.tab]}
+          tabs={tabs}
+        />
+      </TabSelectContext.Provider>
+    </div>
+  )
 }
 
 export default Studio
