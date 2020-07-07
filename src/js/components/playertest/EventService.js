6c24d63fe692cd4159d691d848a08bb27fe887c9
Abdullah <aalthawr@gmail.com>
2020-07-07 22:12:17 +0800

    add hooks for events

---
+++ src/js/components/playertest/EventService.js
@@ -0,0 +1,43 @@
+import { EventEmitter } from "events"
+
+export const CANPLAY = "canplay"
+export const PLAY = "play"
+export const PAUSE = "pause"
+export const SEEKING = "seeking"
+export const SEEKED = "seeked"
+export const ENDED = "ended"
+
+export const initEmitter = () => {
+  const eventEmitter = new EventEmitter()
+  eventEmitter.on(CANPLAY, (data, currentTime) =>
+    console.log("canPlay", data, currentTime)
+  )
+  eventEmitter.on(PLAY, (data, currentTime) =>
+    console.log("playing", data, currentTime)
+  )
+  eventEmitter.on(PAUSE, (data, currentTime, timeRange) =>
+    console.log("pausing", data, currentTime, timeRangesToArray(timeRange))
+  )
+  eventEmitter.on(SEEKING, (data, currentTime) =>
+    console.log("seeking", data, currentTime)
+  )
+  eventEmitter.on(SEEKED, (data, currentTime) =>
+    console.log("seeked", data, currentTime)
+  )
+  eventEmitter.on(ENDED, (data, currentTime, timeRange) =>
+    console.log("ended", data, currentTime, timeRangesToArray(timeRange))
+  )
+
+  return eventEmitter
+}
+
+const timeRangesToArray = (time_ranges) => {
+  const ranges = []
+  for (let i = 0; i < time_ranges.length; i++) {
+    let range = {}
+    range.start = Math.round(time_ranges.start(i))
+    range.end = Math.round(time_ranges.end(i))
+    ranges.push(range)
+  }
+  return ranges
+}
--- src/js/components/playertest/PlayerContainer.js
+++ src/js/components/playertest/PlayerContainer.js
@@ -2,11 +2,20 @@ import React, { useEffect, useRef, useState } from "react"
 import axios from "axios"
 import "shaka-player/dist/controls.css"
 import ShakaPlayer from "../macro/components/ShakaPlayer"
+import {
+  CANPLAY,
+  ENDED,
+  initEmitter,
+  PAUSE,
+  PLAY,
+  SEEKED,
+  SEEKING,
+} from "./EventService"
 const PlayerContainer = ({ video }) => {
   const [playUrl, setPlayUrl] = useState("")
   const [error, setError] = useState("")
   const [loading, setLoading] = React.useState(true)
-  // const controllerRef = useRef(null);
+  const controllerRef = useRef()
 
   useEffect(() => {
     if (video && video.id) {
@@ -22,12 +31,102 @@ const PlayerContainer = ({ video }) => {
     }
   }, [video])
 
+  useEffect(() => {
+    if (!loading) {
+      const videoElement = controllerRef.current.getVideoElement()
+      console.log("what is video element", videoElement)
+      console.log("DURATIOn", videoElement.duration)
+      const eventEmitter = initEmitter()
+      videoElement.addEventListener("loadedmetadata", (event) =>
+        console.log("duration", videoElement.duration)
+      )
+      videoElement.addEventListener("canplay", (event) =>
+        eventEmitter.emit(CANPLAY, event, videoElement.currentTime)
+      )
+      videoElement.addEventListener("play", (event) =>
+        eventEmitter.emit(PLAY, event, videoElement.currentTime)
+      )
+      videoElement.addEventListener("pause", (event) =>
+        eventEmitter.emit(
+          PAUSE,
+          event,
+          videoElement.currentTime,
+          videoElement.played
+        )
+      )
+      videoElement.addEventListener("seeking", (event) =>
+        eventEmitter.emit(SEEKING, event, videoElement.currentTime)
+      )
+      videoElement.addEventListener("seeked", (event) =>
+        eventEmitter.emit(SEEKED, event, videoElement.currentTime)
+      )
+      videoElement.addEventListener("ended", (event) =>
+        eventEmitter.emit(
+          ENDED,
+          event,
+          videoElement.currentTime,
+          videoElement.played
+        )
+      )
+
+      // videoElement.addEventListener("pause", (event)=> console.log(` start ${videoElement.played.start(0)} end ${videoElement.played.end(0)}`))
+
+      // // videoElement.addEventListener('loadedmetadata', function() {
+      // //   videoElement.currentTime = 5;
+      // // }, false);
+      // videoElement.addEventListener("seeking", (state)=>console.log("seeking", state))
+      // videoElement.addEventListener("seeked", (state)=>console.log(`seeked played: ${JSON.stringify(videoElement.played)} - currentTime${videoElement.currentTime}`, state))
+      // videoElement.addEventListener("canplay", (state)=>console.log("canplay", state))
+      // videoElement.addEventListener("ended", (state)=>console.log("end", videoElement.played.length))
+      // videoElement.addEventListener("timeupdate", (state)=>console.log("timeupdate", state))
+
+      return () => {
+        videoElement.removeEventListener("loadedmetadata", (event) =>
+          console.log("duration", videoElement.duration)
+        )
+        videoElement.removeEventListener("canplay", (event) =>
+          eventEmitter.emit(CANPLAY, event, videoElement.currentTime)
+        )
+        videoElement.removeEventListener("play", (event) =>
+          eventEmitter.emit(PLAY, event, videoElement.currentTime)
+        )
+        videoElement.removeEventListener("pause", (event) =>
+          eventEmitter.emit(
+            PAUSE,
+            event,
+            videoElement.currentTime,
+            videoElement.played
+          )
+        )
+        videoElement.removeEventListener("seeking", (event) =>
+          eventEmitter.emit(SEEKING, event, videoElement.currentTime)
+        )
+        videoElement.removeEventListener("seeked", (event) =>
+          eventEmitter.emit(SEEKED, event, videoElement.currentTime)
+        )
+        videoElement.removeEventListener("ended", (event) =>
+          eventEmitter.emit(
+            ENDED,
+            event,
+            videoElement.currentTime,
+            videoElement.played
+          )
+        )
+      }
+    }
+  }, [loading])
+
   return (
     <div>
       {error ? (
         <div>{error}</div>
       ) : (
-        <div> {!loading && <ShakaPlayer autoplay={false} src={playUrl} />}</div>
+        <div>
+          {" "}
+          {!loading && (
+            <ShakaPlayer autoplay={false} src={playUrl} ref={controllerRef} />
+          )}
+        </div>
       )}
     </div>
   )
